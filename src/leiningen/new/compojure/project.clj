4286edb4aad560aa23c98f1f7e590ad8dd9f99cd
James Reeves <jreeves@weavejester.com>
2012-03-30 21:32:43 +0100

    Added plugins, deps and ring handler to project.clj template

--- src/leiningen/new/compojure/project.clj
+++ src/leiningen/new/compojure/project.clj
@@ -2,4 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "1.0.1"]]
+  :plugins [[lein-ring "0.6.2"]]
+  :ring {:handler {{name}}.handler/app})
