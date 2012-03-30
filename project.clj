ef06f51cea552c5df1263a6f61b49282e4af1b2a
James Reeves <jreeves@weavejester.com>
2012-03-30 22:30:06 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject compojure-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Compojure project template for Leiningen"
+  :url "https://github.com/weavejester/compojure-template"
   :eval-in-leiningen true
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
