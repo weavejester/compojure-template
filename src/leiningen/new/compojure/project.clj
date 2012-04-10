a7a18b918bbb9b7c9be4661ba0deab808a28c536
James Reeves <jreeves@weavejester.com>
2012-04-10 14:40:45 +0100

    Updated Compojure and Lein-Ring versions

--- src/leiningen/new/compojure/project.clj
+++ src/leiningen/new/compojure/project.clj
@@ -2,6 +2,6 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "1.0.1"]]
-  :plugins [[lein-ring "0.6.2"]]
+                 [compojure "1.0.2"]]
+  :plugins [[lein-ring "0.6.3"]]
   :ring {:handler {{name}}.handler/app})
