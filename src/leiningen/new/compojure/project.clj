bab641ba627727a6783f556ffdc685d2e0850ab3
James Reeves <jreeves@weavejester.com>
2012-07-13 17:54:40 +0100

    Updated default Clojure to 1.4.0

--- src/leiningen/new/compojure/project.clj
+++ src/leiningen/new/compojure/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler {{name}}.handler/app})
