6b647553523d068e7addf0b547685364a7e3382d
James Reeves <jreeves@weavejester.com>
2012-11-20 23:05:02 +0000

    Fixed lein-ring deps

--- src/leiningen/new/compojure/project_lein1.clj
+++ src/leiningen/new/compojure/project_lein1.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]]
-  :plugins [[lein-ring "0.7.6"]]
+  :plugins [[lein-ring "0.7.5"]]
   :ring {:handler {{name}}.handler/app}
   :dev-dependencies [[ring-mock "0.1.3"]])
