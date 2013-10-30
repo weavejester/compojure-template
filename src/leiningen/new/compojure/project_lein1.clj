b5a9d7701e17d830b74743c1cac793feb2a8b271
James Reeves <jreeves@weavejester.com>
2013-10-30 02:00:53 +0000

    Updated Compojure dependencies

--- src/leiningen/new/compojure/project_lein1.clj
+++ src/leiningen/new/compojure/project_lein1.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler {{name}}.handler/app}
   :dev-dependencies [[ring-mock "0.1.5"]])
