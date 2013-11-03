cadccfff80bab37c6f91414f99ecd766ed8b7201
James Reeves <jreeves@weavejester.com>
2013-11-03 06:26:04 +0000

    Updated lein-ring to 0.8.8

--- src/leiningen/new/compojure/project_lein1.clj
+++ src/leiningen/new/compojure/project_lein1.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.7"]]
+  :plugins [[lein-ring "0.8.8"]]
   :ring {:handler {{name}}.handler/app}
   :dev-dependencies [[javax.servlet/servlet-api "2.5"]
                      [ring-mock "0.1.5"]])
