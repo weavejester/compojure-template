307c067a836c68d149cef1be0366a3fb27e2f401
James Reeves <jreeves@weavejester.com>
2014-01-04 16:11:50 +0000

    Updated lein-ring to 0.8.9

--- src/leiningen/new/compojure/project_lein2.clj
+++ src/leiningen/new/compojure/project_lein2.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.8"]]
+  :plugins [[lein-ring "0.8.9"]]
   :ring {:handler {{name}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
