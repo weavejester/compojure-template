9514745ed9bec727e0e6d16e1def51b0d51554b6
Rodney Folz <rodney@rodneyfolz.com>
2014-03-25 20:22:35 -0700

    Update lein-ring to 0.8.10.

--- src/leiningen/new/compojure/project_lein2.clj
+++ src/leiningen/new/compojure/project_lein2.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.9"]]
+  :plugins [[lein-ring "0.8.10"]]
   :ring {:handler {{name}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
