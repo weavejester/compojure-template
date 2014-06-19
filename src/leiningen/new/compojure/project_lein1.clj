09520cf5e19fdba119d5bd9c3f5a577cc27f1b4f
James Reeves <jreeves@weavejester.com>
2014-06-19 21:45:48 +0100

    Update plugin and dependency versions

--- src/leiningen/new/compojure/project_lein1.clj
+++ src/leiningen/new/compojure/project_lein1.clj
@@ -1,9 +1,9 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
   :ring {:handler {{name}}.handler/app}
   :dev-dependencies [[javax.servlet/servlet-api "2.5"]
                      [ring-mock "0.1.5"]])
