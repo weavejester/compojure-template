129134f46a9f2ae3ca9c326134365ee9b852ea96
James Reeves <jreeves@weavejester.com>
2014-12-12 02:34:25 +0000

    Update Compojure dependency to 1.3.1

--- resources/leiningen/new/compojure/project.clj
+++ resources/leiningen/new/compojure/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.0"]
+                 [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler {{namespace}}.handler/app}
