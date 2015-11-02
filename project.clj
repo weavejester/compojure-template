aabce41dbb484f374756c627c2ab132ee4322054
James Reeves <jreeves@weavejester.com>
2015-11-02 17:18:08 +0000

    Release 0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure/lein-template "0.4.2"
+(defproject compojure/lein-template "0.4.3"
   :description "Compojure project template for Leiningen"
   :url "https://github.com/weavejester/compojure-template"
   :eval-in-leiningen true
