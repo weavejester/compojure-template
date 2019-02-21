ab3aae3435bff3f8421e0c29bc6d6ef821f32378
Robert Corro <robert.corro@guaranteedrate.com>
2019-02-21 16:03:56 -0600

    modified the version number of the Lein Ring to from 4 to 5.

--- resources/leiningen/new/compojure/project.clj
+++ resources/leiningen/new/compojure/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.4"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler {{namespace}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
