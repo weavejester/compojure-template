6081135a7f7a0cafa0d519c57c699c91f13af86f
Ari Paasonen <paasar@gmail.com>
2019-11-29 13:48:36 +0200

    Fix the order of expected and actual in handler_test.clj template

--- resources/leiningen/new/compojure/handler_test.clj
+++ resources/leiningen/new/compojure/handler_test.clj
@@ -6,9 +6,9 @@
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= 200 (:status response)))
+      (is (= "Hello World" (:body response)))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= 404 (:status response))))))
